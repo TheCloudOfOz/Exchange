a1ecb445f936f92417aaef0e248bc70612240fa8
Simon Oz <138634174+TheCloudOfOz@users.noreply.github.com>
2023-10-24 05:47:03 +1100

    Create Install.ps1

---
+++ Install.ps1
@@ -0,0 +1,22 @@
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+
+$Parameters = @{
+    Confirm      = $false 
+    Force        = $true
+    Scope        = "CurrentUser"
+    AllowClobber = $true
+    Repository   = "PSGallery"
+}
+
+# Microsoft
+Install-Module @Parameters -Name 'Microsoft.PowerShell.SecretManagement'
+Install-Module @Parameters -Name 'Microsoft.PowerShell.SecretStore'
+Install-Module @Parameters -Name 'Microsoft.Graph'
+
+# Microsoft Online
+Install-Module @Parameters -Name 'MSOnline'
+Install-Module @Parameters -Name 'AzureAD'
+Install-Module @Parameters -Name 'ExchangeOnlineManagement'
+Install-Module @Parameters -Name 'Microsoft.Online.SharePoint.PowerShell'
+Install-Module @Parameters -Name 'MicrosoftTeams'
